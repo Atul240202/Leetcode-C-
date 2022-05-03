815f023c5217ab1027edf254da7a5a7920f7dc67
Atul Jha <69789941+Atul240202@users.noreply.github.com>
2022-05-03 21:19:15 +0530

    Update Container With Most Water.cpp

--- Container With Most Water.cpp
+++ Container With Most Water.cpp
@@ -19,3 +19,7 @@ public:
 
 
 ///Question Link: https://leetcode.com/problems/container-with-most-water/
+
+/*Approach Explaination: Question mai hume nikalna hai ki konse time prr rectangular/square container ki capacity sbse jyada hogi on the basis of array input to simple isme 
+formula lagega Area= Length*Breadth. Abb question is simple Length=Height[i] or Height[j] inme se jo bhi chota ho & Breadth = j-i and abb krte jao multiply jiska area bada hga
+woh answer hoga*/
