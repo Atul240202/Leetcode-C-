85416e985e5242c7df29f104b04abea15204b80e
Atul Jha <69789941+Atul240202@users.noreply.github.com>
2022-05-19 17:37:34 +0530

    Create 75. Sort Colors.cpp

---
+++ 75. Sort Colors.cpp
@@ -0,0 +1,22 @@
+///USing Sorting Algorithm
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i,j;
+        for (i =0; i<nums.size();i++)
+        {
+            for(j=i+1; j<nums.size();j++)
+            {
+                if (nums[i]>nums[j])
+                {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j]=temp;
+                }
+            }
+        }
+        
+    }
+};
+
